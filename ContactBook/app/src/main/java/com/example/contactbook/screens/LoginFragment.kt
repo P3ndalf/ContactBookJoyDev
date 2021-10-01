332c1b65371fb047330ef02b56b0321e63c77577
P3ndalf <52200518+P3ndalf@users.noreply.github.com>
2021-10-01 23:45:56 +0300

    Now buttoms works correctly

--- ContactBook/app/src/main/java/com/example/contactbook/screens/LoginFragment.kt
+++ ContactBook/app/src/main/java/com/example/contactbook/screens/LoginFragment.kt
@@ -2,6 +2,7 @@ package com.example.contactbook.screens
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.contactbook.R
@@ -9,8 +10,9 @@ import com.example.contactbook.R
 class LoginFragment : Fragment(R.layout.fragment_login) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle? ){
         super.onViewCreated(view, savedInstanceState)
-        view.postDelayed({
-            findNavController().navigate(R.id.action_loginFragment_to_mainFragment)
-        }, 3000)
+
+        view.findViewById<Button>(R.id.register_fragment).setOnClickListener{
+            findNavController().navigate(R.id.action_loginFragment_to_registerFragment)
+        }
     }
 }
--- ContactBook/app/src/main/java/com/example/contactbook/screens/RegisterFragment.kt
+++ ContactBook/app/src/main/java/com/example/contactbook/screens/RegisterFragment.kt
@@ -2,6 +2,7 @@ package com.example.contactbook.screens
 
 import android.os.Bundle
 import android.view.View
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.example.contactbook.R
@@ -9,8 +10,9 @@ import com.example.contactbook.R
 class RegisterFragment : Fragment(R.layout.fragment_register) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle? ){
         super.onViewCreated(view, savedInstanceState)
-        view.postDelayed({
-            findNavController().navigate(R.id.action_registerFragment_to_mainFragment)
-        }, 3000)
+
+        view.findViewById<Button>(R.id.login_fragment).setOnClickListener{
+            findNavController().navigate(R.id.action_registerFragment_to_loginFragment)
+        }
     }
 }
