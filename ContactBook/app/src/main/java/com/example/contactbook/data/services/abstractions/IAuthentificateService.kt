e0bc162cce0273ea93a94837ec2cb01dfa9f9e10
P3ndalf <52200518+P3ndalf@users.noreply.github.com>
2021-11-30 12:56:22 +0300

    refactoring

--- ContactBook/app/src/main/java/com/example/contactbook/data/services/abstractions/IAuthentificateService.kt
+++
@@ -1,7 +0,0 @@
-package com.example.contactbook.data.services.abstractions
-
-interface IAuthentificateService {
-    fun isUserExists(email : String, password : String) : Boolean
-
-    fun loginUser(email : String, password: String)
-}
